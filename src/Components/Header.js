a53ee25ea35f0c69fab804a221fee1ddc767074a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-03 17:42:06 +0900

    Routes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { ReactComponent as Search } from "assets/search.svg";
 
 const Header = styled.div``;
 
 export default () => (
   <Header>
-    <Link to="/movies">Movies</Link>
+    <Link to="/">Movies</Link>
     <Link to="/tv">TV</Link>
+    <Link to="/search">
+      <Search />
+    </Link>
   </Header>
 );
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
-import Home from "../Routes/Home";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
 import Header from "./Header";
+import Search from "../Routes/Search";
 
 export default () => (
   <Router>
     <>
       <Header />
       <Route path="/" exact component={Home} />
+      <Route path="/tv" component={TV} />
+      <Route path="/search" component={Search} />
     </>
   </Router>
 );
