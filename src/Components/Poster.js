fbf959e05ffb84343305c16ba9664fe9e311bac1
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-05 19:04:03 +0900

    Poster

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -1,8 +1,45 @@
 import React from "react";
+import styled from "styled-components";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const Poster = ({ imageUrl, rating, name, year, seasons, id }) => null;
+const Container = styled.div``;
+
+const Image = styled.div``;
+
+const Votes = styled.span``;
+
+const Data = styled.div``;
+
+const Name = styled.span``;
+
+const Year = styled.span``;
+
+const DataColumn = styled.div``;
+
+const Poster = ({ imageUrl, rating, name, year, seasons, id }) => (
+  <Container>
+    <Image bgImage={imageUrl}>
+      <Votes>
+        <span role="img" aria-label="Stars">
+          ⭐️
+        </span>{" "}
+        {rating}/10
+      </Votes>
+    </Image>
+    <Data>
+      <DataColumn>
+        <Name>{name}</Name>
+        <Year>{year}</Year>
+      </DataColumn>
+      {seasons && (
+        <DataColumn>
+          {seasons === 1 ? "1 season" : `${seasons} seasons.`}
+        </DataColumn>
+      )}
+    </Data>
+  </Container>
+);
 
 Poster.propTypes = {
   id: PropTypes.number.isRequired,
