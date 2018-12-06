087c946b115542a6a230c786700aae7312d98d9c
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-12-06 12:40:50 +0900

    Detail

--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -6,14 +6,14 @@ import ErrorText from "../../Components/ErrorText";
 
 const Container = styled.div`
   position: relative;
-  padding: 50px;
+  padding: 80px;
   height: calc(100vh - 45px);
 `;
 
 const Background = styled.div`
   width: 100%;
   height: 100%;
-  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+  background-image: linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7)),
     url(${props => props.bgImage});
   background-size: 100%;
   background-position: center center;
@@ -28,6 +28,7 @@ const Content = styled.div`
   z-index: 9;
   width: 100%;
   height: 100%;
+  display: flex;
 `;
 
 const Poster = styled.div`
@@ -37,6 +38,34 @@ const Poster = styled.div`
   background-size: cover;
   border-radius: 7px;
   background-position: center center;
+`;
+
+const Data = styled.div`
+  color: rgba(255, 255, 255, 0.9);
+  margin-left: 20px;
+  font-size: 12px;
+  width: 70%;
+`;
+
+const Name = styled.span`
+  font-size: 42px;
+  color: white;
+`;
+
+const InfoRow = styled.div`
+  margin-top: 20px;
+`;
+
+const InfoItem = styled.span``;
+
+const Bullet = styled.span`
+  margin: 0 10px;
+`;
+
+const Overview = styled.p`
+  margin-top: 20px;
+  width: 50%;
+  line-height: 2;
 `;
 
 const DetailPresenter = ({ loading, result, error }) =>
@@ -53,6 +82,50 @@ const DetailPresenter = ({ loading, result, error }) =>
         <Poster
           bgImage={`https://image.tmdb.org/t/p/w500${result.poster_path}`}
         />
+        <Data>
+          <Name>
+            {result.original_name
+              ? result.original_name
+              : result.original_title}
+          </Name>
+          <InfoRow>
+            <InfoItem>
+              {result.first_air_date
+                ? result.first_air_date.substring(0, 4)
+                : result.release_date.substring(0, 4)}
+            </InfoItem>
+            <Bullet>•</Bullet>
+            <InfoItem>
+              {result.episode_run_time
+                ? result.episode_run_time
+                : result.runtime}{" "}
+              min
+            </InfoItem>
+            <Bullet>•</Bullet>
+            <InfoItem>
+              {result.genres &&
+                result.genres.map((genre, index) => {
+                  if (index + 1 === result.genres.length) {
+                    return genre.name;
+                  } else {
+                    return `${genre.name} / `;
+                  }
+                })}
+            </InfoItem>
+            <Bullet>•</Bullet>
+            {result.status && <InfoItem>{result.status}</InfoItem>}
+            <Bullet>•</Bullet>
+            {result.vote_average && (
+              <InfoItem>
+                <span role="img" aria-label="rating">
+                  ⭐️
+                </span>{" "}
+                {result.vote_average} / 10
+              </InfoItem>
+            )}
+          </InfoRow>
+          {result.overview && <Overview>{result.overview}</Overview>}
+        </Data>
       </Content>
     </Container>
   );
