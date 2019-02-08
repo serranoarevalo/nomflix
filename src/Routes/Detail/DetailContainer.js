78aa70366f3ba0b554ffba12cb82ce98f6a8fb24
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-02-08 18:22:43 +0900

    Detail Refactor

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,51 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DetailPresenter from "./DetailPresenter";
 import { movies, tv } from "../../api";
 
-export default class extends React.Component {
-  state = {
-    loading: true,
-    result: null,
-    error: null
-  };
+export default ({
+  location: { pathname },
+  match: {
+    params: { id }
+  }
+}) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+  const [result, setResult] = useState(null);
 
-  componentDidMount = () => {
-    const {
-      location: { pathname },
-      match: {
-        params: { id }
-      }
-    } = this.props;
+  const getDetail = async () => {
     const isMovie = pathname.includes("movie");
-    this.isMovie = isMovie;
-    this.getDetail(id);
-  };
-
-  getDetail = async id => {
     try {
       let result;
-      if (this.isMovie) {
+      if (isMovie) {
         const { data } = await movies.getMovie(id);
         result = data;
       } else {
         const { data } = await tv.getShow(id);
         result = data;
       }
-      this.setState({
-        result
-      });
+      setResult(result);
     } catch {
-      this.setState({
-        error: "Can't find what you're looking for"
-      });
+      setError("Can't find what you're looking for");
     } finally {
-      this.setState({
-        loading: false
-      });
+      setLoading(false);
     }
   };
-  render() {
-    const { loading, result, error } = this.state;
-    return <DetailPresenter loading={loading} result={result} error={error} />;
-  }
-}
+
+  useEffect(() => {
+    getDetail();
+  }, []);
+
+  return <DetailPresenter loading={loading} result={result} error={error} />;
+};
